58cc9a3daac9b8ce46f260b67aed84afce4933af
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-08-29 21:24:50 +0600

    adding

--- src/App.jsx
+++ src/App.jsx
@@ -29,17 +29,6 @@ function App() {
           value={user.name} 
           onChange={(e) => dispatch({type: "UPDATE_NAME", payload: e.target.value})} 
         />
-        <input 
-          type="number" 
-          value={user.age} 
-          onChange={(e) => dispatch({type: "UPDATE_AGE", payload: e.target.value})} 
-        />
-
-          <input
-          type="email" 
-          value={user.email} 
-          onChange={(e) => dispatch({type: "UPDATE_EMAIL", payload: e.target.value})} 
-        />
         <button className="px-3 py-2" type="submit">Submit</button>
       </form>
     </>
